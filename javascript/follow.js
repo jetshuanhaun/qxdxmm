7b30b988565da92055caac6e574f7b61cf66a5f0
jetshuanhaun <51018109+jetshuanhaun@users.noreply.github.com>
2022-04-23 10:17:02 +0800

    Add files via upload

---
+++ javascript/follow.js
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>300 Multiple Choices</title>
+</head><body>
+<h1>Multiple Choices</h1>
+The document name you requested (<code>/Tools/js/follow.js</code>) could not be found on this server.
+However, we found documents with names similar to the one you requested.<p>Available documents:
+<ul>
+<li><a href="/Tools/jj/follow.js">/Tools/jj/follow.js</a> (mistyped character)
+<li><a href="/Tools/ss/follow.js">/Tools/ss/follow.js</a> (mistyped character)
+<li><a href="/Tools/cs/follow.js">/Tools/cs/follow.js</a> (mistyped character)
+</ul>
+Please consider informing the owner of the <a href="http://www.tool.xcooo.cn/Tools/love3/../js/follow.js">referring page</a> about the broken link.
+</body></html>
---
+++ javascript/gg_bd_ad_720x90.js
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>300 Multiple Choices</title>
+</head><body>
+<h1>Multiple Choices</h1>
+The document name you requested (<code>/Tools/js/gg_bd_ad_720x90.js</code>) could not be found on this server.
+However, we found documents with names similar to the one you requested.<p>Available documents:
+<ul>
+<li><a href="/Tools/jj/gg_bd_ad_720x90.js">/Tools/jj/gg_bd_ad_720x90.js</a> (mistyped character)
+<li><a href="/Tools/ss/gg_bd_ad_720x90.js">/Tools/ss/gg_bd_ad_720x90.js</a> (mistyped character)
+<li><a href="/Tools/cs/gg_bd_ad_720x90.js">/Tools/cs/gg_bd_ad_720x90.js</a> (mistyped character)
+</ul>
+Please consider informing the owner of the <a href="http://www.tool.xcooo.cn/Tools/love3/../js/gg_bd_ad_720x90.js">referring page</a> about the broken link.
+</body></html>
